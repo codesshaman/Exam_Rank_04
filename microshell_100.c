89d0dc082b56f5eae6f095a02b812e2afbde2e46
Jimmie Leslee <jleslee@pr-d3.kzn.21-school.ru>
2022-07-06 19:57:51 +0300

    4

--- microshell_100.c
+++ microshell_100.c
@@ -2,7 +2,7 @@
 #include <sys/wait.h>
 #include <string.h>
 
-int	ft_putstr_fd2(char *str, char *arg){
+int	ft_putstr(char *str, char *arg){
 	while(*str)
 		write(2, str++, 1);
 	if(arg)
@@ -12,12 +12,12 @@ int	ft_putstr_fd2(char *str, char *arg){
 	return 1;
 }
 
-int ft_execute(char *av[], int i, int fd, char *env[]){
+int error(char *av[], int i, int fd, char *env[]){
     av[i] = NULL;
     dup2(fd, STDIN_FILENO);
     close(fd);
     execve(av[0], av, env);
-    return(ft_putstr_fd2("error: cannot execute ", av[0]));
+    return(ft_putstr("error: cannot execute ", av[0]));
 }
 
 int	main(int argc, char *argv[], char *env[]){
@@ -37,14 +37,14 @@ int	main(int argc, char *argv[], char *env[]){
 			i++;
 		if(strcmp(argv[0], "cd") == 0){ //cd
 			if(i != 2)
-				ft_putstr_fd2("error: cd: bad arguments", NULL);
+				ft_putstr("error: cd: bad arguments", NULL);
 			else if(chdir(argv[1]) != 0)
-				ft_putstr_fd2("error: cd: cannot change directory to ", argv[1]	);
+				ft_putstr("error: cd: cannot change directory to ", argv[1]	);
 		}
 		else if(i != 0 && (argv[i] == NULL || strcmp(argv[i], ";") == 0)){ //exec in stdout
 			pid = fork();
 			if(pid == 0){
-				if(ft_execute(argv, i, tmp_fd, env))
+				if(error(argv, i, tmp_fd, env))
 					return 1;
 			} else {
 				close(tmp_fd);
@@ -60,7 +60,7 @@ int	main(int argc, char *argv[], char *env[]){
 				dup2(fd[1], STDOUT_FILENO);
 				close(fd[0]);
 				close(fd[1]);
-				if(ft_execute(argv, i, tmp_fd, env))
+				if(error(argv, i, tmp_fd, env))
 					return 1;
 			} else {
 				close(fd[1]);
