0f5780ed8dd3bb53bc8964a93fff777db892d3a8
Nick <90702473+codesshaman@users.noreply.github.com>
2022-07-19 18:38:09 +0300

    Update microshell_100.c

--- microshell_100.c
+++ microshell_100.c
@@ -3,66 +3,66 @@
 #include <string.h>
 
 int	ft_putstr(char *str, char *arg){
-	while(*str)
+	while (*str)
 		write(2, str++, 1);
-	if(*arg)
+	if (arg)
 		while(*arg)
 			write(2, arg++, 1);
 	write(2, "\n", 1);
-	return 1;
+	return (1);
 }
 
-int error(char **av, int i, int fd, char **env){
-    av[i] = NULL;
-    dup2(fd, STDIN_FILENO);
-    close(fd);
-    execve(av[0], av, env);
-    return(ft_putstr("error: cannot execute ", av[0]));
+int ft_execute(char *av[], char *env[], int i, int tmp_fd){
+	av[i] = NULL;
+	dup2(tmp_fd, STDIN_FILENO);
+	close(tmp_fd);
+	execve(av[0], av, env);
+	return (ft_putstr("error: cannot execute ", av[0]));
 }
 
-int	main(int argc, char **argv, char **env){
+int	main(int argc, char *av[], char *env[]){
 	int	i;
 	int pid;
 	int fd[2];
 	int tmp_fd;
-	(void)argc;	// is needed in exam, because the exam tester compiles with -Wall -Wextra -Werror
+	(void)argc;
 
 	pid = 0;
 	i = 0;
 	tmp_fd = dup(STDIN_FILENO);
-	while(argv[i] && argv[i + 1]){ //check if the end is reached
-		argv = &argv[i + 1];	//the new argv start after the ; or |
-		i = 0; //count until we have all informations to execute the next child;
-		while(argv[i] && strcmp(argv[i], ";") && strcmp(argv[i], "|"))
+	while (av[i] && av[i + 1]){
+		av = &av[i + 1];
+		i = 0;
+		while (av[i] && strcmp(av[i], ";") && strcmp(av[i], "|"))
 			i++;
-		if(strcmp(argv[0], "cd") == 0){ //cd
-			if(i != 2)
+		if (strcmp(av[0], "cd") == 0){
+			if (i != 2)
 				ft_putstr("error: cd: bad arguments", NULL);
-			else if(chdir(argv[1]) != 0)
-				ft_putstr("error: cd: cannot change directory to ", argv[1]	);
+			else if (chdir(av[1]) != 0)
+				ft_putstr("error: cd: cannot change directory to ", av[1]	);
 		}
-		else if(i != 0 && (argv[i] == NULL || strcmp(argv[i], ";") == 0)){ //exec in stdout
+		else if (i != 0 && (av[i] == NULL || strcmp(av[i], ";") == 0)){
 			pid = fork();
-			if(pid == 0){
-				if(error(argv, i, tmp_fd, env))
-					return 1;
-			} else {
+			if ( pid == 0){
+				if (ft_execute(av, env, i, tmp_fd))
+					return (1);
+			}else{
 				close(tmp_fd);
 				while(waitpid(-1, NULL, WUNTRACED) != -1)
 					;
 				tmp_fd = dup(STDIN_FILENO);
 			}
 		}
-		else if(i != 0 && strcmp(argv[i], "|") == 0){ //pipe
+		else if(i != 0 && strcmp(av[i], "|") == 0){
 			pipe(fd);
 			pid = fork();
 			if(pid == 0){
 				dup2(fd[1], STDOUT_FILENO);
 				close(fd[0]);
 				close(fd[1]);
-				if(error(argv, i, tmp_fd, env))
-					return 1;
-			} else {
+				if (ft_execute(av, env, i, tmp_fd))
+					return (1);
+			}else{
 				close(fd[1]);
 				close(tmp_fd);
 				tmp_fd = fd[0];
